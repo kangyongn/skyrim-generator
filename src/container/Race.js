df827c6c3a5033c989892cba426997cb6c8f913a
github username <j.yongnamkang@gmail.com>
2019-09-12 22:38:36 -0400

    created Randomizer and Race container

---
+++ src/container/Race.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Race = props => {
+  return (
+    <div>
+      <h1>Race</h1>
+      <button>Roll 1</button>
+      <button>Roll 2</button>
+    </div>
+  )
+}
+
+export default Race;
---
+++ src/container/Randomizer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Race from './Race';
+
+const Randomizer = props => {
+  return (
+    <div>
+      <Race/>
+    </div>
+  )
+}
+
+export default Randomizer
