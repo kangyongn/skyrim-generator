8362e0d3b978529d76adcc6e702530b98f480592
github username <j.yongnamkang@gmail.com>
2019-09-13 00:04:02 -0400

    created resource folder for data

--- src/component/Race.js
+++ src/component/Race.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 
+import {raceDictionary} from '../resource/Data';
+
 const Race = props => {
   const [rollOne, setRollOne] = useState({
     value: 0,
@@ -9,21 +11,6 @@ const Race = props => {
     value: 0,
     rolled: false
   });
-
-  const raceDictionary = {
-    1: 'Not Possible',
-    2: 'Roll Again',
-    3: 'Altmer',
-    4: 'Argonian',
-    5: 'Bosmer',
-    6: 'Breton',
-    7: 'Dunmer',
-    8: 'Imperial',
-    9: 'Khajit',
-    10: 'Nord',
-    11: 'Orc',
-    12: 'Redguard'
-  }
 
   const handleRollOne = () => {
     setRollOne({
---
+++ src/resource/Data.js
@@ -0,0 +1,14 @@
+  export const raceDictionary = {
+    1: 'Not Possible',
+    2: 'Roll Again',
+    3: 'Altmer',
+    4: 'Argonian',
+    5: 'Bosmer',
+    6: 'Breton',
+    7: 'Dunmer',
+    8: 'Imperial',
+    9: 'Khajit',
+    10: 'Nord',
+    11: 'Orc',
+    12: 'Redguard'
+  }
