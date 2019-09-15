261a386a4674e4795a0261c188ba9bce348a4fb7
github username <j.yongnamkang@gmail.com>
2019-09-15 14:17:02 -0400

    organized Data

--- src/resource/Data.js
+++ src/resource/Data.js
@@ -1,3 +1,84 @@
+export const ethicDictionary = {
+  1: ['Chaotic Good', ['Seeks to aid others and protect them from harm', ' Values freedom and individual choice.', 'Is not bound by promise made, but does not deceive without reason.', 'Distrust authority.', 'Is loyal to friends and good teammates, but not necessarily to their methods.']],
+  2: ['Neutral Good', ['Seeks to aid others and protect them from harm.', 'Is not concerned about honor as much as virtue.', 'Is not deceptive, but might break promises if circumstances change.', 'Accepts benevolent authority.', 'Is loyal to friends and good teammates.']],
+  3: ['Lawful Good', ['Seeks to aid others and protect them from harm.', 'Values just laws and self-dicipline.', 'Is honest and honors promises made.', 'Respects legitimate authority.', 'Is loyal to friends and teammates.']],
+  4: ['Chaotic Neutral', ['Tends to let others fend for themselves.', 'Values freedom.', 'Is not bound by promises made.', 'Rejects authority.', 'Is loyal only to oneself.']],
+  5: ['True Neutral', ['May treat others well or poorly as circumstances dictate.', 'Neither values nor disregards honor.', 'Is usually not habitually deceptive, but may break promises if circumstances change.', 'May work with or against authority.', 'May be loyal or faithless, depending on the character and circumstances.']],
+  6: ['Lawful Neutral', ['May punish the guilty and reward the innocent.', 'Values law, order, and self-discipline.', 'Will keep promises if possible.', 'Respects authority.', 'Is loyal to teammates.']],
+  7: ['Chaotic Evil', ['Has no qualms about hurting or killing anyone; some even kill innocent for fun.', 'Rejects honor and self-discipline as weakness.', "Will say anything to accomplish goals; keeping one's word is done only when opportune.", "Accepts another's authority only when forced.", 'Will betray friends and teammates to further selfish goals.']],
+  8: ['Neutral Evil', ['Has no qualms about hurting or killing anyone.', 'Places no value on honor.', 'Might keep promises to allies, but does not feel bound to do so.', "Might accept another's authority in name only.", 'May betray friends to advance a cause or to benefit themself.']],
+  9: ['Lawful Evil', ['Has no qualms about hurting or killing anyone, but may prefer to do so indirectly.' , 'Values consistency and reliability, particularly when they can be used.', 'Prefers to not outright lie; deceit comes when others draw inferences beyond what is explicitly said.', "Accepts another's authority until it can be usurped.", 'Is outwardly loayal to teammates unless (or until) betrayal can be justified.']],
+}
+
+export const flawDictionary = {
+  1: ['Pride', 'Selects boastful speech options, obsessed with achieving 100 in a skill by any means necessary(crime, exploits, etc).'],
+  2: ['Greed', 'Hoards septims and one or two other miscellaneous items; has a goal of a certain level of wealth.'],
+  3: ['Lust', 'Easily seduced by Daedra and other bad influences of the opposite sex.'],
+  4: ['Gluttony', 'Uses drugs, good, and alcohol excessively.'],
+  5: ['Wrath', 'Quick tempered and always resorts to violence; may kill or assault NPCs for petty reason.'],
+  6: ['Envy', 'Obsessed with gaining the same power or status of an existing NPC(guildmaster, thane, Jarl, etc).'],
+}
+
+export const interestDictionary = {
+  1: ['Not Possible'],
+  2: ['Agriculture', 'Learns from farmers and alchemists; collects plants and fungi for alchemy; interested in adding a garden to your home.'],
+  3: ['Architecture', 'Studies the structural design of buildings, forts, houses, keeps, and ruins. May be obsessed with Dwarven or Nordic Ruins.'],
+  4: ['Beastiary', "Collects Herbane's Beastiary books and studies the habits and dwellings of Skyrim's creatures."],
+  5: ['Barter & Economics', 'Worships Zenithar, seeks/makes haggling potions, imposes rules and restrictions on what can be traded with merchants.'],
+  6: ['Culinary Arts', 'Samples and rates the food and drinks at various inns. Invents new recipes. Plans lavish meals that incoprporate alchemy ingredients(venison smothered in troll fat, snowberry jam, etc).'],
+  7: ['Fitness & Exercise', 'Starts each day with a morning sprint, works out at sawmills (lifting logs, chopping wood), swims regularly.'],
+  8: ['History', 'Collects books on history and takes on quests pertaining to historical events.'],
+  9: ['Horseback Riding', 'Takes horseback rides at sunrise or sunset when relaxing in a town or village.'],
+  10: ['Moutain Climbing', "Obsessed with climbing Skyrim's highest mountains."],
+  11: ['Politics', "Interested in people's opinions about the Empire, the civil war, and political figures in Skyrim."],
+  12: ['Theology', 'Studies Aedric and Daedric religions.'],
+}
+
+export const locationDictionary = {
+  1: 'Not Possible',
+  2: 'Falkreath',
+  3: 'Ivarstead',
+  4: 'Karthwasten',
+  5: 'Kynesgrove',
+  6: 'Morthal',
+  7: 'Markarth',
+  8: 'Riften',
+  9: 'Solitude',
+  10: 'Anywhere You Want',
+  11: 'Windhelm',
+  12: "Other(Darkwater Crossing, Rorikstead, Shot's Stone, or Stonehills)",
+}
+
+export const motivationDictionary = {
+  1: ['Not Possible'],
+  2: ['Research', 'You cam to Skyrim to learn about a specific topic unique to the region.'],
+  3: ['Mystery', 'You are looking for clues about the sudden death of a friend or family member.'],
+  4: ['Vegeance', 'You crossed the border to find and kill the person who murdered a friend or family member.'],
+  5: ['Family Heirloom', 'You are searching for a weapon or piece of jewlery that once belonged to your family.'],
+  6: ['Exile', 'You were case out of your home region due to a crime or allegation of a crime.'],
+  7: ['Redemption', 'You are trying to right a wrong you commited in the past.'],
+  8: ['Recovery', 'You suffered from a debilitating disease or injury that caused your skills to diminish. You came to Skyrim to train yourself back to strength.'],
+  9: ['War', 'You came to Skyrim specifically to join the Stormcloaks or the Empire.'],
+  10: ['Love', 'You are trying to locate a lost love, who could be alive and well or long dead.'],
+  11: ['Pilgrimage', 'You came to see the shrines, the temples, the Gildergreen, or to collect Daedric artifacts.'],
+  12: ['Weath & Power', 'You are seeking power(Thanehood, guild leadership, etc) and/or money(jewels, houses, gold).'],
+}
+
+export const occupationDictionary = {
+  1: 'Not Possible',
+  2: 'Assassin',
+  3: 'Alchemist',
+  4: 'Blacksmith',
+  5: 'Chef',
+  6: 'Farmer',
+  7: 'Fisherman',
+  8: 'Hunter',
+  9: 'Merchant',
+  10: 'Miner',
+  11: 'Soldier',
+  12: 'Thief',
+}
+
 export const raceDictionary = {
   1: 'Not Possible',
   2: 'Roll Again',
@@ -41,85 +122,4 @@ export const virtueDictionary = {
   4: ['Temperance', 'Does not use drugs(Skooma, Moon Sugar, Sleeping Tree Sap); eats and drinks in moderation.'],
   5: ['Diligence', 'Does not steal or loot the dead; has a non-violent, honorable occupation.'],
   6: ['Kindness', 'Does not harm or steal from friends or neighborsl only harms "evil" NPCs.'],
-}
-
-export const flawDictionary = {
-  1: ['Pride', 'Selects boastful speech options, obsessed with achieving 100 in a skill by any means necessary(crime, exploits, etc).'],
-  2: ['Greed', 'Hoards septims and one or two other miscellaneous items; has a goal of a certain level of wealth.'],
-  3: ['Lust', 'Easily seduced by Daedra and other bad influences of the opposite sex.'],
-  4: ['Gluttony', 'Uses drugs, good, and alcohol excessively.'],
-  5: ['Wrath', 'Quick tempered and always resorts to violence; may kill or assault NPCs for petty reason.'],
-  6: ['Envy', 'Obsessed with gaining the same power or status of an existing NPC(guildmaster, thane, Jarl, etc).'],
-}
-
-export const occupationDictionary = {
-  1: 'Not Possible',
-  2: 'Assassin',
-  3: 'Alchemist',
-  4: 'Blacksmith',
-  5: 'Chef',
-  6: 'Farmer',
-  7: 'Fisherman',
-  8: 'Hunter',
-  9: 'Merchant',
-  10: 'Miner',
-  11: 'Soldier',
-  12: 'Thief',
-}
-
-export const locationDictionary = {
-  1: 'Not Possible',
-  2: 'Falkreath',
-  3: 'Ivarstead',
-  4: 'Karthwasten',
-  5: 'Kynesgrove',
-  6: 'Morthal',
-  7: 'Markarth',
-  8: 'Riften',
-  9: 'Solitude',
-  10: 'Anywhere You Want',
-  11: 'Windhelm',
-  12: "Other(Darkwater Crossing, Rorikstead, Shot's Stone, or Stonehills)",
-}
-
-export const interestDictionary = {
-  1: ['Not Possible'],
-  2: ['Agriculture', 'Learns from farmers and alchemists; collects plants and fungi for alchemy; interested in adding a garden to your home.'],
-  3: ['Architecture', 'Studies the structural design of buildings, forts, houses, keeps, and ruins. May be obsessed with Dwarven or Nordic Ruins.'],
-  4: ['Beastiary', "Collects Herbane's Beastiary books and studies the habits and dwellings of Skyrim's creatures."],
-  5: ['Barter & Economics', 'Worships Zenithar, seeks/makes haggling potions, imposes rules and restrictions on what can be traded with merchants.'],
-  6: ['Culinary Arts', 'Samples and rates the food and drinks at various inns. Invents new recipes. Plans lavish meals that incoprporate alchemy ingredients(venison smothered in troll fat, snowberry jam, etc).'],
-  7: ['Fitness & Exercise', 'Starts each day with a morning sprint, works out at sawmills (lifting logs, chopping wood), swims regularly.'],
-  8: ['History', 'Collects books on history and takes on quests pertaining to historical events.'],
-  9: ['Horseback Riding', 'Takes horseback rides at sunrise or sunset when relaxing in a town or village.'],
-  10: ['Moutain Climbing', "Obsessed with climbing Skyrim's highest mountains."],
-  11: ['Politics', "Interested in people's opinions about the Empire, the civil war, and political figures in Skyrim."],
-  12: ['Theology', 'Studies Aedric and Daedric religions.'],
-}
-
-export const motivationDictionary = {
-  1: ['Not Possible'],
-  2: ['Research', 'You cam to Skyrim to learn about a specific topic unique to the region.'],
-  3: ['Mystery', 'You are looking for clues about the sudden death of a friend or family member.'],
-  4: ['Vegeance', 'You crossed the border to find and kill the person who murdered a friend or family member.'],
-  5: ['Family Heirloom', 'You are searching for a weapon or piece of jewlery that once belonged to your family.'],
-  6: ['Exile', 'You were case out of your home region due to a crime or allegation of a crime.'],
-  7: ['Redemption', 'You are trying to right a wrong you commited in the past.'],
-  8: ['Recovery', 'You suffered from a debilitating disease or injury that caused your skills to diminish. You came to Skyrim to train yourself back to strength.'],
-  9: ['War', 'You came to Skyrim specifically to join the Stormcloaks or the Empire.'],
-  10: ['Love', 'You are trying to locate a lost love, who could be alive and well or long dead.'],
-  11: ['Pilgrimage', 'You came to see the shrines, the temples, the Gildergreen, or to collect Daedric artifacts.'],
-  12: ['Weath & Power', 'You are seeking power(Thanehood, guild leadership, etc) and/or money(jewels, houses, gold).'],
-}
-
-export const ethicDictionary = {
-  1: ['Chaotic Good', ['Seeks to aid others and protect them from harm', ' Values freedom and individual choice.', 'Is not bound by promise made, but does not deceive without reason.', 'Distrust authority.', 'Is loyal to friends and good teammates, but not necessarily to their methods.']],
-  2: ['Neutral Good', ['Seeks to aid others and protect them from harm.', 'Is not concerned about honor as much as virtue.', 'Is not deceptive, but might break promises if circumstances change.', 'Accepts benevolent authority.', 'Is loyal to friends and good teammates.']],
-  3: ['Lawful Good', ['Seeks to aid others and protect them from harm.', 'Values just laws and self-dicipline.', 'Is honest and honors promises made.', 'Respects legitimate authority.', 'Is loyal to friends and teammates.']],
-  4: ['Chaotic Neutral', ['Tends to let others fend for themselves.', 'Values freedom.', 'Is not bound by promises made.', 'Rejects authority.', 'Is loyal only to oneself.']],
-  5: ['True Neutral', ['May treat others well or poorly as circumstances dictate.', 'Neither values nor disregards honor.', 'Is usually not habitually deceptive, but may break promises if circumstances change.', 'May work with or against authority.', 'May be loyal or faithless, depending on the character and circumstances.']],
-  6: ['Lawful Neutral', ['May punish the guilty and reward the innocent.', 'Values law, order, and self-discipline.', 'Will keep promises if possible.', 'Respects authority.', 'Is loyal to teammates.']],
-  7: ['Chaotic Evil', ['Has no qualms about hurting or killing anyone; some even kill innocent for fun.', 'Rejects honor and self-discipline as weakness.', "Will say anything to accomplish goals; keeping one's word is done only when opportune.", "Accepts another's authority only when forced.", 'Will betray friends and teammates to further selfish goals.']],
-  8: ['Neutral Evil', ['Has no qualms about hurting or killing anyone.', 'Places no value on honor.', 'Might keep promises to allies, but does not feel bound to do so.', "Might accept another's authority in name only.", 'May betray friends to advance a cause or to benefit themself.']],
-  9: ['Lawful Evil', ['Has no qualms about hurting or killing anyone, but may prefer to do so indirectly.' , 'Values consistency and reliability, particularly when they can be used.', 'Prefers to not outright lie; deceit comes when others draw inferences beyond what is explicitly said.', "Accepts another's authority until it can be usurped.", 'Is outwardly loayal to teammates unless (or until) betrayal can be justified.']],
 }
